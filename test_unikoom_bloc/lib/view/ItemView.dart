3c87f6f83be12e65b411625feb8f2dbb98393dd0
Balajahe <balajahe@gmail.com>
2021-02-07 12:48:46 +0300

    dev

--- test_unikoom_bloc/lib/view/ItemView.dart
+++ test_unikoom_bloc/lib/view/ItemView.dart
@@ -10,29 +10,21 @@ class ItemView extends StatelessWidget {
   @override
   build(context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(200, 100, 100, 100),
-      appBar: AppBar(
-        title: Text(item.title),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 25, bottom: 25, left: 20, right: 20),
-        child: Card(
-          elevation: 5,
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(item.title, style: TextStyle(fontSize: 18)),
-                  Container(
-                      height: 8,
-                      padding: EdgeInsets.only(top: 3, bottom: 3),
-                      child: Container(color: Colors.grey)),
-                  Text(item.text, style: TextStyle(fontSize: 16)),
-                ],
-              ),
-            ),
+      //backgroundColor: Color.fromARGB(200, 100, 100, 100),
+      appBar: AppBar(title: Text(item.title)),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20, bottom: 20, left: 15, right: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(item.title, style: TextStyle(fontSize: 18)),
+              Container(
+                  height: 12,
+                  padding: EdgeInsets.only(top: 5, bottom: 5),
+                  child: Container(color: Colors.grey)),
+              Text(item.text, style: TextStyle(fontSize: 16)),
+            ],
           ),
         ),
       ),
