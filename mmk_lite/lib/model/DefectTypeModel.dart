3045decbd16f2e8e4095396ace5e88c7d50d5f58
Balajahe <balajahe@gmail.com>
2021-03-09 12:53:10 +0300

    dev

--- mmk_lite/lib/model/DefectTypeModel.dart
+++ mmk_lite/lib/model/DefectTypeModel.dart
@@ -1,31 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DefectTypeState {
-  List<String> all = [];
-  String filter = '';
-  bool waiting = false;
+  List<String> all;
+  String filter;
+  bool waiting;
   String error;
+  DefectTypeState(
+    this.all, {
+    this.filter = '',
+    this.waiting = false,
+    this.error = '',
+  });
 }
 
 class DefectTypeModel extends Cubit<DefectTypeState> {
   List<String> _all = [];
   String _filter = '';
 
-  DefectTypeModel() : super(DefectTypeState()..waiting = true) {
+  DefectTypeModel() : super(DefectTypeState([], waiting: true)) {
     _load();
   }
 
   void _load() async {
     await Future.delayed(Duration(seconds: 1));
     _all = List.from(['Ничего не получается', 'Фатальный недостаток', 'Просто дефект']);
-    emit(DefectTypeState()..all = _all);
+    emit(DefectTypeState(_all));
   }
 
   void filter(String s) {
     _filter = s.toLowerCase();
-    emit(DefectTypeState()
-      ..filter = _filter
-      ..all = _all.where((v) => v.toLowerCase().contains(_filter = s.toLowerCase())).toList());
+    emit(DefectTypeState(
+      _all.where((v) => v.toLowerCase().contains(_filter = s.toLowerCase())).toList(),
+      filter: _filter,
+    ));
   }
 
   void clearFilter() {
