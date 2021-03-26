16f4f34177c8ee42c4b13417efe068e7139ee4dd
Balajahe <balajahe@gmail.com>
2021-03-26 14:05:51 +0300

    dev

---
+++ mmk_lite/deploy-web.sh
@@ -0,0 +1,5 @@
+rm ../../balajahe.github.io/flutter/mmk_lite/* -r -f
+cp ./build/web/* ../../balajahe.github.io/flutter/mmk_lite/ -r -f
+cd ../../balajahe.github.io/flutter/mmk_lite/
+git commit -a -m dev
+git push
